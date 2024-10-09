2991d6e06444de20cc9bf8e877c1c741f9b20de7
CsOHQAQ <529353999@qq.com>
2024-10-09 17:19:17 -0600

    *Fix bug: Keeping engage combat after lose

--- Assets/Scripts/Map/RoomControler.cs
+++ Assets/Scripts/Map/RoomControler.cs
@@ -66,7 +66,7 @@ public class RoomControler : MonoBehaviour
                 foreach (MonsterStats monster in monsters)
                 {
                     monster.RefreshAwareness(isViewedByPlayer);
-                    if (monster.awareLevel == MonsterStats.AwareLevel.Awared)
+                    if (monster.CurAwareLevel == MonsterStats.AwareLevel.Awared)
                     {
                         EngageCombat();
                         return;
@@ -123,6 +123,7 @@ public class RoomControler : MonoBehaviour
 
     void LeaveCombat(bool result)
     {
+        isRefreshing = false;
         inCombat = false;
         GameControl.Game.Player.GetComponent<PlayerMovement>().CanMove = true;
         if (result)//Player Wins
@@ -134,7 +135,8 @@ public class RoomControler : MonoBehaviour
             //Player Loses, Please call the new day
             foreach(var mo in monsters)
             {
-                mo.awareLevel = 0;
+                mo.Awareness = 0;
+                mo.CurAwareLevel = MonsterStats.AwareLevel.NotAwared;
             }
         }
     }
