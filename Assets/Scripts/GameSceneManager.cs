bc3be3b31e4f07d7eee55c881050c6bea8b9da25
Carter Block <carterpblock@gmail.com>
2024-10-09 14:55:03 -0600

    Added loading scene functionality.

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -8,6 +8,19 @@ public class GameSceneManager : MonoBehaviour
 
     public static GameSceneManager Instance;
 
+    [Header("Transition Properties")]
+    [SerializeField]
+    private float minLoadDelay;
+
+    [SerializeField]
+    private float maxLoadDelay;
+
+    [SerializeField]
+    private string loadingSceneName;
+
+    private bool canTransition;
+    
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -22,13 +35,38 @@ public class GameSceneManager : MonoBehaviour
         }
     }
 
+    public void Start()
+    {
+        canTransition = true;
+    }
+
     public void QuitGame()
     {
         Application.Quit();
     }
 
-    public void TransitionToScene(int index)
+    public void TransitionToScene(string sceneName)
     {
-        SceneManager.LoadScene(index);
+
+        if (canTransition)
+        {
+            canTransition = false;
+
+            float randomDelay = Random.Range(minLoadDelay, maxLoadDelay);
+
+            StartCoroutine(LoadSceneTransition(randomDelay, sceneName));
+        }
+    }
+
+    private IEnumerator LoadSceneTransition(float delay, string sceneName)
+    {
+
+        SceneManager.LoadScene(loadingSceneName);
+
+        yield return new WaitForSeconds(delay);
+
+        SceneManager.LoadScene(sceneName);
+
+        canTransition = true;
     }
 }
