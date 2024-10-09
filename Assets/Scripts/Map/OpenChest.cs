c5647345aa1fa448099b6e1121985facb3d6b0b2
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2024-10-08 22:09:49 -0600

    Oops

--- Assets/Scripts/Map/OpenChest.cs
+++ Assets/Scripts/Map/OpenChest.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class OpenChest : InteractableObject
 {
-    [SerializeField] bool randomize;
+    [SerializeField] bool randomize = true;
     [SerializeField] List<Item_ScriptableObject> chestItems;
     [SerializeField] private int maxCount = 5;
 
     public bool isOpen = false;
     public bool CanOpen = true;
+
+    private float chestOpeness = 0f; // Range from 0 (closed) to 1 (fully open)
 
     private void Awake()
     {
@@ -18,13 +20,14 @@ public class OpenChest : InteractableObject
             PopulateChest();
         }
 
-
     }
 
     public override void Interact(object args = null)
     {
         if (args != null && args is InteractDetect)
         {
+            InteractDetect interactDetect = (InteractDetect)args;
+            chestOpeness = Mathf.Clamp(interactDetect.ScrollValue, 0f, 1f); // Clamps openness between 0 and 1
         }
     }
 
