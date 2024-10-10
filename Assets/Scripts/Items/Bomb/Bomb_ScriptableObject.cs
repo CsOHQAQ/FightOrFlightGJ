486a1df186fe6290c59186337586c932a67ff180
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2024-10-09 18:19:00 -0600

    fix bomb scriptable object

---
+++ Assets/Scripts/Items/Bomb/Bomb_ScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Items/Bomb", order = 3)]
+public class Bomb_ScriptableObject : Item_ScriptableObject
+{
+
+}
