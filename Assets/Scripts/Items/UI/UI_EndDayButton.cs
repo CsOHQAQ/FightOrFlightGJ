ca89fb60a48371f1341f574f87f6a15f4a635ab7
Carter Block <carterpblock@gmail.com>
2024-10-09 23:07:11 -0600

    Day transition works!!!!

--- Assets/Scripts/Items/UI/UI_EndDayButton.cs
+++ Assets/Scripts/Items/UI/UI_EndDayButton.cs
@@ -6,6 +6,7 @@ public class UI_EndDayButton : MonoBehaviour
 {
     public void EndDayButtonOnClick()
     {
+        Debug.Log("EndDayClicked");
         UI_InventoryManager.Instance.HideCanvas();
         DayManager.Instance.GoToNextDay();
     }
