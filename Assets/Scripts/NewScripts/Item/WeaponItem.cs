6146fda413944af392e4a1b38f3b958a0a001156
yhharryc <yh.harryc@hotmail.com>
2024-12-12 14:02:42 -0700

    fix

--- Assets/Scripts/NewScripts/Item/WeaponItem.cs
+++ Assets/Scripts/NewScripts/Item/WeaponItem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class WeaponItem : BaseItem, IEquipable, IActivatable {
     public event Action<IItem, ICharacter> OnEquipped;
